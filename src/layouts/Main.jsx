bbe7d703235205633ac5090dd858c75c7f16ace0
sohelranalive <sohel.cse37@gmail.com>
2023-05-04 19:34:18 +0600

    redirect to top, scroll to top functionality added

--- src/layouts/Main.jsx
+++ src/layouts/Main.jsx
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Header from '../components/Header/Header';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 import Footer from '../components/Footer/Footer';
 
 const Main = () => {
+
+    const { pathname } = useLocation();
+
+    // Automatically scrolls to top whenever pathname changes
+    useEffect(() => {
+        window.scrollTo({ top: 0, behavior: 'smooth' });
+    }, [pathname]);
+
     return (
         <div className='bg-custom-color'>
             <Header></Header>
